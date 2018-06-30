a98fb7b3b817da8d4d2f8b35d44d29f9fbe29e6a
SpiritQAQ <444042073@qq.com>
2018-06-30 16:47:40 +0800

    dev server 1，but style.css 2

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,2 +1,3 @@
 import scss from "../scss/app.scss"
 console.log(2333)
+console.log('haha')
--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,7 @@ var HtmlwebpackPlugin = require('html-webpack-plugin')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
-  entry: {
-    js : __dirname+'/javascripts/main.js',
-    css : __dirname+'/scss/app.scss'
-  },
+  entry: __dirname+'/javascripts/main.js',
   //入口文件
   output: {
     filename: 'app.js',
@@ -16,7 +13,8 @@ module.exports = {
   devServer: {
     contentBase: path.join(__dirname),
     // inline:true, 
-    port:8080
+    port:8080,
+    open:true 
   },
   devtool: 'source-map',
   module: {
@@ -34,26 +32,28 @@ module.exports = {
       {
         test: /\.(scss|sass)$/, 
         loader: "style-loader!css-loader!sass-loader" 
-      }
-      // {
+      },
+      {
       //   test: /\.scss$/,
+      // loader: ExtractTextPlugin.extract("style", 'css!sass')
       //   use: ExtractTextPlugin.extract(
       //     {
       //       fallback:'css-loader',
       //       use:['style-loader','css-loader','sass-loader']   
       //     })//样式分离
-      // }
+      }
 
       
     ]
   },
   plugins: [
-    // new HtmlwebpackPlugin({
-    //     title: 'Hello World app'
-    // })//在dist目录下自动生成index.html，指定其title
-    // ,
-    new ExtractTextPlugin({
-      filename : "dist/style.css"
-    })//提取出来的样式放在style.css文件中
-],
+    new HtmlwebpackPlugin({
+        title: 'server dev index',
+        template:"index.html" //指定模板
+    })//在dist目录下自动生成index.html，指定其title
+    ,
+//     new ExtractTextPlugin({
+//       filename : "dist/style.css"
+//     })//提取出来的样式放在style.css文件中
+  ],
 }
