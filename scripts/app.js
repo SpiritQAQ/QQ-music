6d9e96be609ed3d004080869092fe856b16127de
SpiritQAQ <444042073@qq.com>
2018-08-18 03:32:21 +0800

    tab事件委托 完成

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,5 @@
 import scss from "../scss/app.scss"
 import Recommend from "./recommend.js"
-
+import "./tab.js"
 
 let recommend = new Recommend(document.querySelector(".rec-wrapper")).launch()
---
+++ scripts/tab.js
@@ -0,0 +1,22 @@
+//事件委托
+document.addEventListener("click",function(event){
+  let target = event.target
+
+  if(target.dataset.role !== "tab")  return
+
+  [].forEach.call(target.parentElement.children, tab =>{
+    //[].forEach.call() == Array.prototype.forEach.call()
+    tab.classList.remove("tab-active")
+  })
+  target.classList.add('tab-active')
+
+  let pages = document.querySelector("#content-wrapper").children
+
+  Array.prototype.forEach.call(pages,page =>{
+    page.classList.add("hide")
+    if(page.dataset.tab == target.dataset.nav){
+      page.classList.remove("hide")
+    }  
+  })
+  
+})
